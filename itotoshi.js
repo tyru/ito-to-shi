0b3385d23065ecf81b709b02b7257647abca7ce1
tyru <tyru.exe@gmail.com>
2016-06-05 23:24:00 +0900

    Add TODO comment

--- itotoshi.js
+++ itotoshi.js
@@ -1,4 +1,15 @@
 window.ItoToShi = (function() {
+
+  // TODO: Support the all following screen widths like the ones defined by Bootstrap
+  // * Extra small devices (phones, less than 768px)
+  //   * No media query since this is the default in Bootstrap
+  // * Small devices (tablets, 768px and up)
+  //   * @media (min-width: @screen-sm-min) { ... }
+  // * Medium devices (desktops, 992px and up)
+  //   * @media (min-width: @screen-md-min) { ... }
+  // * Large devices (large desktops, 1200px and up)
+  //   * @media (min-width: @screen-lg-min) { ... }
+
   function getInitVars() {
     return {
       svgDS: {
